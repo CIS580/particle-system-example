a0a484c70aaf544651c357556ffdced466d423a0
Nathan Bean <nhbean@ksu.edu>
2021-03-30 12:28:51 -0500

    Initial commit

---
+++ ParticleSystemExample/ParticleSystemExampleGame.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ParticleSystemExample
+{
+    /// <summary>
+    /// An example game demonstrating the use of particle systems
+    /// </summary>
+    public class ParticleSystemExampleGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        /// <summary>
+        /// Constructs an instance of the game
+        /// </summary>
+        public ParticleSystemExampleGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Initializes the game
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Loads the game content
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// Updates the game.  Called every frame of the game loop.
+        /// </summary>
+        /// <param name="gameTime">The time in the game</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Draws the game.  Called every frame of the game loop.
+        /// </summary>
+        /// <param name="gameTime">The time in the game</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ParticleSystemExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParticleSystemExample
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ParticleSystemExampleGame())
+                game.Run();
+        }
+    }
+}
